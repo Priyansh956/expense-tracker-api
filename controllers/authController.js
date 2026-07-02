c782844ebe83a23e1ce285595fb3b5ace1425b9a
Priyansh Kumar <pkd09052006@gmail.com>
2026-07-02 13:00:22 +0530

    Feat: JWT Authentication setup for login as well as signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,7 @@
+const bcrypt = require('bcrypt');
+
 const {User} = require('../models/userModels'); 
+const {setUser} = require('../services/auth');
 
 async function signUp(req, res){
     const body = req.body;
@@ -24,19 +27,77 @@ async function signUp(req, res){
         "success": false,
     });
 
-    await User.create({
-        email: req.body.email,
-        password: req.body.password,
-        categories: req.body.categories,
+    const email = body.email;
+    const existingUser = await User.findOne({email});
+
+    if(existingUser){
+        return res.status(409).json({
+            "message": "An account already exists with this email id",
+            "success": false,
+        })
+    }
+
+    //Password hashing
+    const salt = await bcrypt.genSalt(10);
+    const encryptedPassword = await bcrypt.hash(req.body.password, salt);
+    try{
+        await User.create({
+            email: req.body.email,
+            password: encryptedPassword,
+            categories: req.body.categories,
+        });
+    
+        return res.status(201).json({
+            "message": "Successful",
+            // "message": securePassword,
+            "success": true,
+        });
+    }
+    catch(err){
+        if (err.code === 11000) {
+        return res.status(409).json({
+            "success": false,
+            "message": "Email already exists",
+        });
+        }
+
+        res.status(500).json({
+            "success": false,
+            "message": "Server error",
+        });
+    }
+}
+
+async function userLogin(req, res){
+    const {email} = req.body;
+    
+
+    const user = await User.findOne({email});
+    if(!user) return res.status(404).json({
+        "message": "Invalid email or password",
+        "success": false
     });
 
-    return res.status(201).json({
-        // "message": "Successful",
-        "message": "A new user is created successfully",
-        "success": true,
+    const comparison = await bcrypt.compare(
+        req.body.password,
+        user.password
+    );
+    
+    if(!comparison)return res.status(401).json({
+        "message": "Invalid email or password",
+        "success": false,
     });
+
+    const token = setUser(user);
+
+    return res.status(200).json({
+        "message": "User found",
+        "success": true,
+        token,
+    })
 }
 
 module.exports = {
     signUp,
+    userLogin,
 };
